a602a9f57631ed8a0f5823816bf2b2276ff5f09f
aayush <ayushwasnik999@gmail.com>
2023-05-30 15:28:01 +0530

    demo

--- 1.js
+++ 1.js
@@ -3,3 +3,7 @@ const Deme=()=>{
 
     console.log("hi")
 }
+
+const handleclick = () => {
+    console.log("hi this is dummy")
+}
---
+++ dummy.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dummy() {
+  return (
+    <div>
+        <h1>hey this is ayush</h1>
+    </div>
+  )
+}
+
+export default Dummy;
